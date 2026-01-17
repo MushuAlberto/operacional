0395b110517de849ba20eaab98730ce1aa095bd7
MushuAlberto <crtapiae@gmail.com>
2026-01-17 02:30:36 -0300

    Updated MainMenu component to use the official SQM logo URL

--- components/MainMenu.tsx
+++ components/MainMenu.tsx
@@ -1,3 +1,14 @@
-import logo from '../public/logo.svg';
+import React from 'react';
+import './MainMenu.css';
 
-// Rest of your component code
+const MainMenu = () => {
+    return (
+        <div className="main-menu">
+            <img src="https://www.example.com/sqm-logo.png" alt="SQM Logo" /> {/* Replace with the actual SQM logo URL */}
+            <button className="button">Llegada de Equipos</button>
+            <button className="button">Informe Operativo</button>
+        </div>
+    );
+};
+
+export default MainMenu;
